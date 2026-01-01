d785ae4ccdc34d55ee002811c81641ea434e7c99
Dinh Tan Hieu <dinhtanhieu2005@gmail.com>
2026-01-01 19:58:34 +0700

    feat: main page with lang, theme

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -8,11 +8,13 @@ const Layout = () => {
   const theme = useAppSelector((state) => state.theme.theme);
 
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex h-screen flex-col">
       <Header />
-      {/* <div className="flex-1 bg-zinc-50 text-black"> */}
+
       <div
-        className={`flex-1 ${theme === "light" ? "bg-zinc-50 text-black" : "bg-zinc-800 text-white"}`}
+        className={`flex flex-1 overflow-hidden ${
+          theme === "light" ? "bg-zinc-50 text-black" : "bg-zinc-800 text-white"
+        }`}
       >
         <Outlet />
       </div>
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,67 @@
+import Avatar from "@/assets/itachi_dongo-nonbg.png";
+
+import { useAppSelector } from "@/hooks/hooks";
+
 const MainPage = () => {
+  const theme = useAppSelector((state) => state.theme.theme);
+  const language = useAppSelector((state) => state.language.language);
+
   return (
-    <div className="h-full">
-      <div className="">HomePage</div>
+    <div className="flex h-full w-full items-center justify-center">
+      <div className="flex h-full w-full max-w-6xl gap-x-20">
+        {/* LEFT */}
+        <div className="flex flex-1 items-center justify-end">
+          <div
+            className={`flex h-[80%] w-[70%] items-center justify-center overflow-hidden rounded-t-full ${
+              theme === "light"
+                ? "bg-linear-to-br from-zinc-200 to-zinc-400"
+                : "bg-linear-to-br from-zinc-500 to-zinc-300"
+            }`}
+          >
+            <img
+              src={Avatar}
+              alt="avatar"
+              className="h-full w-full object-contain"
+            />
+          </div>
+        </div>
+
+        {/* RIGHT */}
+        <div className="flex flex-1 flex-col items-center justify-center gap-y-6">
+          <div className="text-2xl">
+            {language === "vi"
+              ? "Xin chào! Tôi là"
+              : "Hello everyone! My name is"}
+          </div>
+          <div className="text-6xl font-bold">DINHHHIUU</div>
+          <div className="text-2xl">
+            {language === "vi" ? "KỸ SƯ PHẦN MỀM" : "SOFTWARE ENGINEER"}
+          </div>
+          <div className="flex gap-x-4">
+            {/* Introduce */}
+            <button
+              className={`rounded-2xl px-6 py-2.5 font-medium transition-all duration-200 hover:scale-105 active:scale-95 ${
+                theme === "light"
+                  ? "border border-zinc-400 text-zinc-800 hover:bg-zinc-200"
+                  : "border border-white/70 text-white hover:bg-white/10"
+              } `}
+            >
+              {language === "vi" ? "Giới thiệu" : "Introduce"}
+            </button>
+
+            {/* Download CV */}
+            <button
+              className={`rounded-2xl px-6 py-2.5 font-medium transition-all duration-200 hover:scale-105 active:scale-95 ${
+                theme === "light"
+                  ? "bg-zinc-800 text-white hover:bg-zinc-700"
+                  : "bg-white text-zinc-900 hover:bg-zinc-200"
+              } `}
+            >
+              {language === "vi" ? "Tải CV" : "Download CV"}
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
